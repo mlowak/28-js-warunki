42137e6ab44f2a7872f917a5ef86385c36a88686
mlowak <martyna.lowak@gmail.com>
2020-07-20 09:13:56 +0200

    koniec

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('Martyna');
+
+let numer = 12;
+
+if (numer > 10) {
+    console.log('Spełniony warunek: numer > 10');
+} else if (numer < 10) {
+    console.log('Spełniony warunek: numer < 10');
+}
